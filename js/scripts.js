f0b7c9760bee3265cd5fd8de19413aa0c92ccd91
chrissch.dev <66461609+ChrisSch-dev@users.noreply.github.com>
2024-06-23 20:31:58 +0800

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@
 	const vapp = new Vue({
 		el: "#app",
 		data: {
-			users: [
+			team: [
 				{ name: "Uthsho", detail: "Founder", avatar: "https://cdn.discordapp.com/avatars/747870498221719592/a_ec11c86942d7d03147864c24880165ab.gif?size=4096" },
-				{ name: "chissch_dhil.dev", detail: "Lead Developer", avatar: "https://cdn.discordapp.com/avatars/592663451978039298/b5ace6ed0d2a2f4b07ba9a207c4e6f76.png?size=4096" },
+				{ name: "chrissch_dhil.dev", detail: "Lead Developer", avatar: "https://cdn.discordapp.com/avatars/592663451978039298/b5ace6ed0d2a2f4b07ba9a207c4e6f76.png?size=4096" },
 				{ name: "oBrent", detail: "Web Developer", avatar: "https://cdn.discordapp.com/avatars/718489111765319692/52492c9631e68fe6902257ec664750c9.png?size=4096" },
 				{ name: "\"Shimeji\" Rin", detail: "Bot Developer", avatar: "https://cdn.discordapp.com/avatars/586913853804249090/a_81ed93fe08e29db876f0bbfbc67abd1e.gif?size=4096" },
 				{ name: "Aniket", detail: "Bot Developer", avatar: "https://cdn.discordapp.com/avatars/474255126228500480/122498b2abd7493fc075b249aef72dc7.png?size=4096" }
@@ -12,12 +12,12 @@
 		},
 	});
 
-	fetch("https://galaxies.silentjungle.tk/devteam")
+	fetch("https://github.com/EpochStudio")
 		.then(function (response) {
 			return response.json();
 		})
 		.then(function (data) {
-			vapp.users = data;
+			vapp.team = data;
 			console.log(data)
 		});
 
