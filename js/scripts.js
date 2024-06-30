8da4b1f18f8f94361d0ce99b9d181c6efee93b77
chrissch.dev <66461609+ChrisSch-dev@users.noreply.github.com>
2024-06-30 16:13:04 +0800

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,10 @@
 		data: {
 			team: [
 				{ name: "Uthsho", detail: "Founder", avatar: "./images/uthsho.gif" },
-				{ name: "chrissch_dhil.dev", detail: "Lead Developer In-charge", avatar: "https://cdn.discordapp.com/avatars/592663451978039298/b5ace6ed0d2a2f4b07ba9a207c4e6f76.png?size=4096" },
-				{ name: "\"Shimeji\" Rin", detail: "Acting Lead Developer", avatar: "https://cdn.discordapp.com/avatars/586913853804249090/a_81ed93fe08e29db876f0bbfbc67abd1e.gif?size=4096" },
-				{ name: "oBrent", detail: "Web Developer", avatar: "https://cdn.discordapp.com/avatars/718489111765319692/52492c9631e68fe6902257ec664750c9.png?size=4096" },
-				{ name: "Aniket", detail: "Bot Developer", avatar: "https://cdn.discordapp.com/avatars/474255126228500480/122498b2abd7493fc075b249aef72dc7.png?size=4096" }
+				{ name: "chrissch_dhil.dev", detail: "Lead Developer In-charge", avatar: "./images/chrissch.png" },
+				{ name: "\"Shimeji\" Rin", detail: "Acting Lead Developer", avatar: "./images/rin.png" },
+				{ name: "oBrent", detail: "Web Developer", avatar: "./images/obrent.png" },
+				{ name: "Aniket", detail: "Bot Developer", avatar: "./images/aniket.png" }
 			],
 		},
 	});
