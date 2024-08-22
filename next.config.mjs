3dfc69a212d883e4a4556d9c519e947a0a534245
syntaxsaviors <bloodbeast312@gmail.com>
2024-08-22 14:53:20 +0530

    Added SyntaxSavior to Team

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,11 @@
 const nextConfig = {
     publicRuntimeConfig: {
         team: [
-            { name: "Uthsho", detail: "Founder", avatar: "./images/uthsho.gif" },
-            { name: "chrissch_dhil.dev", detail: "Lead Developer In-charge", avatar: "./images/chrissch.png" },
-            { name: "\"Shimeji\" Rin", detail: "Acting Lead Developer", avatar: "./images/rin.png" },
-            { name: "Aniket", detail: "Bot Developer", avatar: "./images/aniket.png" }
+            { name: "Uthsho", detail: "Founder", avatar: "/images/uthsho.gif" },
+            { name: "chrissch_dhil.dev", detail: "Lead Developer In-charge", avatar: "/images/chrissch.png" },
+            { name: "\"Shimeji\" Rin", detail: "Acting Lead Developer", avatar: "/images/rin.png" },
+            { name: "Aniket", detail: "Bot Developer", avatar: "/images/aniket.png" },
+            { name: "SyntaxSavior", detail: "Trial Web Developer", avatar: "/images/syntaxsavior.png" }
         ],
     }
 };
