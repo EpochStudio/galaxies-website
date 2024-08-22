d297d0a3f95db91ebf05f1350f96e09ead718d5f
SyntaxSavior <90510895+syntaxsaviors@users.noreply.github.com>
2024-08-22 13:23:35 +0530

    Add files via upload
    Converted to Next.JS 14 and Added Privacy and TOS route
    
    Note: layout.jsx line 21 needs to be updated with domain name

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    publicRuntimeConfig: {
+        team: [
+            { name: "Uthsho", detail: "Founder", avatar: "./images/uthsho.gif" },
+            { name: "chrissch_dhil.dev", detail: "Lead Developer In-charge", avatar: "./images/chrissch.png" },
+            { name: "\"Shimeji\" Rin", detail: "Acting Lead Developer", avatar: "./images/rin.png" },
+            { name: "Aniket", detail: "Bot Developer", avatar: "./images/aniket.png" }
+        ],
+    }
+};
+
+export default nextConfig;
---
+++ src/app/layout.jsx
@@ -0,0 +1,54 @@
+import { Raleway } from 'next/font/google'
+
+import "./bootstrap.css";
+import "./fontawesome-all.css";
+import "./globals.css";
+
+import GoogleScripts from "@/Components/GoogleScripts";
+import Navbar from "@/Components/Navbar";
+
+const font = Raleway({
+  weight: ['400', '600', '700'],
+  subsets: ["latin-ext"],
+  style: ["normal", "italic"]
+});
+
+export const metadata = {
+  title: "Galaxies Discord Bot",
+  description: "Galaxies. A multipurpose utility bot dependable to manage events and performs tasks seamlessly.",
+  authors: [{ name: 'chrissch_dhil.dev' }],
+
+  metadataBase: new URL('http://localhost:3000/'),
+  openGraph: {
+    title: 'Galaxies Discord bot',
+    description: 'Galaxies. A multipurpose utility bot dependable to manage events and performs tasks seamlessly.',
+    url: 'https://galaxies-website.vercel.app/',
+    siteName: 'Galaxies bot',
+    images: '/images/favicon.png',
+    type: 'website',
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={font.className} data-spy="scroll" data-target=".fixed-top">
+        <GoogleScripts />
+
+        <Navbar />
+        {children}
+
+
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/jquery.easing.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/scripts.js"></script>
+        <script src="https://epochstudio.statuspage.io/embed/script.js"></script>
+
+        <script>
+          statusEmbedTest()
+        </script>
+      </body>
+    </html>
+  );
+}
