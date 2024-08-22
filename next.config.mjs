e71f523c00f941d61da98b89928b53623612c3aa
chrissch.dev <66461609+ChrisSch-dev@users.noreply.github.com>
2024-08-22 16:23:39 +0800

    refactor: update file path

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@
 const nextConfig = {
     publicRuntimeConfig: {
         team: [
-            { name: "Uthsho", detail: "Founder", avatar: "./images/uthsho.gif" },
-            { name: "chrissch_dhil.dev", detail: "Lead Developer In-charge", avatar: "./images/chrissch.png" },
-            { name: "\"Shimeji\" Rin", detail: "Acting Lead Developer", avatar: "./images/rin.png" },
-            { name: "Aniket", detail: "Bot Developer", avatar: "./images/aniket.png" }
+            { name: "Uthsho", detail: "Founder", avatar: "./app/images/uthsho.gif" },
+            { name: "chrissch_dhil.dev", detail: "Lead Developer In-charge", avatar: "./app/images/chrissch.png" },
+            { name: "\"Shimeji\" Rin", detail: "Acting Lead Developer", avatar: "./app/images/rin.png" },
+            { name: "Aniket", detail: "Bot Developer", avatar: "./app/images/aniket.png" }
         ],
     }
 };
